5447ca7811c4328a5eacb693a06cbaada16fb626
Ricardo Alvarez <ralvarezlo@miners.utep.edu>
2018-10-15 21:49:25 -0600

    maybe

--- emphaticDemo/framedThreadServer.py
+++ emphaticDemo/framedThreadServer.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import sys, os, socket, params, time
 from threading import Thread
+from threading import Lock
 from framedSock import FramedStreamSock
 
 switchesVarDefaults = (
@@ -30,17 +31,45 @@ class ServerThread(Thread):
         self.fsock, self.debug = FramedStreamSock(sock, debug), debug
         self.start()
     def run(self):
+        fileODone = False
         while True:
             msg = self.fsock.receivemsg()
+            if msg:
+                fileString = msg.decode().replace("\x00", "\n")
+
+                if not fileODone:
+                    auxS = fileString.split("//myname")
+                    if auxS[0] in myDict:
+                        myLock = myDict(auxS[0])
+                        myLock.acquire()
+                    else:
+                        myDict[auxS[0]] = Lock()
+
+                    myPath = os.path.join(os.getcwd()+"/receiving/"+auxS[0])
+                    myFile = open(myPath, "w+")
+                    myFile.write(auxS[1])
+                    fileODone = True
+                
+                myFile.write(fileString)
+            else:
+                myFile.close()
+                break
+            """
             if not msg:
                 if self.debug: print(self.fsock, "server thread done")
                 return
+            """
+
+
+            """
             requestNum = ServerThread.requestCount
             time.sleep(0.001)
             ServerThread.requestCount = requestNum + 1
             msg = ("%s! (%d)" % (msg, requestNum)).encode()
             self.fsock.sendmsg(msg)
-
+            """
+global myDict
+myDict = dict()
 
 while True:
     sock, addr = lsock.accept()
