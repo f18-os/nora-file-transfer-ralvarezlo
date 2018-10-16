11b2987f4147b3888233ab07f2e93c8e84733cd7
Ricardo Alvarez <ralvarezlo@miners.utep.edu>
2018-10-15 19:06:53 -0600

    ini

--- emphaticDemo/framedThreadClient.py
+++ emphaticDemo/framedThreadClient.py
@@ -7,8 +7,19 @@ from framedSock import FramedStreamSock
 from threading import Thread
 import time
 
+myHost = "50001"
+while (1):
+    userAns = input("Use proxy(\"p\") or local host 50001(\"l\")?")
+    if(userAns=="p"):
+        myHost = "50000"
+        break
+    elif(userAns=="l"):
+        myHost = "50001"
+        break
+    else: print("Invalid selection, try again")
+
 switchesVarDefaults = (
-    (('-s', '--server'), 'server', "localhost:50001"),
+    (('-s', '--server'), 'server', "localhost:"+myHost),
     (('-d', '--debug'), "debug", False), # boolean (set if present)
     (('-?', '--usage'), "usage", False), # boolean (set if present)
     )
@@ -36,8 +47,8 @@ class ClientThread(Thread):
         self.serverHost, self.serverPort, self.debug = serverHost, serverPort, debug
         self.start()
     def run(self):
-       s = None
-       for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        s = None
+        for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
            af, socktype, proto, canonname, sa = res
            try:
                print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
@@ -56,19 +67,34 @@ class ClientThread(Thread):
                continue
            break
 
-       if s is None:
+        if s is None:
            print('could not open socket')
            sys.exit(1)
 
-       fs = FramedStreamSock(s, debug=debug)
+        fs = FramedStreamSock(s, debug=debug)
 
+        while (1): 
+            try:
+                fname = input("Please input name: ")
+                myFile = open(fname,'r')
+                break
+            except IOError:
+                print("File doesn't exist, try again.")
 
-       print("sending hello world")
-       fs.sendmsg(b"hello world")
-       print("received:", fs.receivemsg())
+        auxStr = myFile.read().replace("\n", "\0") #replace new lines to null characters
+        myFile.close()
+        auxStr = fname + "//myname" + auxStr  + '\n' #Added the //myname to distinguish between file name and body
+        fs.sendmsg(auxStr.encode())
+        fs.receivemsg() #Just to make sure the message is sent
 
-       fs.sendmsg(b"hello world")
-       print("received:", fs.receivemsg())
+        """
+        print("sending hello world")
+        fs.sendmsg(b"hello world")
+        print("received:", fs.receivemsg())
+
+        fs.sendmsg(b"hello world")
+        print("received:", fs.receivemsg())
+        """
 
 for i in range(100):
     ClientThread(serverHost, serverPort, debug)
